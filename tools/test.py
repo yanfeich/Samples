c215e0736214a44f4dfdb371fc9a03139d937036
Zhai Feiyue <feizhai@habana.ai>
2024-07-30 16:00:25 +0800

    update

--- tools/test.py
+++ tools/test.py
@@ -1,6 +1,6 @@
 import os
-ff = os.listdir('./src')
-ff = [i for i in ff if os.path.isdir(os.path.join('./src', i))]
+ff = os.listdir('../src')
+ff = [i for i in ff if os.path.isdir(os.path.join('../src', i))]
 
 for f in ff:
     print(f'- [{f}](./src/{f}/README.md)')
