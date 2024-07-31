b0800e5623b6f77f036b580b609d893da1ccb76d
yanfeich <yanfei.cheng@intel.com>
2024-07-31 18:17:30 +0800

    update

--- src/softmax/softmax.cpp
+++ src/softmax/softmax.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <synapse_api.h>
 #include <synapse_common_types.hpp>
+#include <perf_lib_layer_params.h>
 #include <cassert>
 #include <iostream>
 #include <vector>
@@ -21,219 +22,128 @@ int main(int argc, char *argv[])
     synStatus status = synInitialize();
     assert(status == synSuccess && "Failed to call  synInitialize()");
 
-    // uint32_t deviceId = 0;
-    // status = synDeviceAcquire(&deviceId, nullptr);
-    // assert(status == synSuccess && "Failed to call  synDeviceAcquire()");
+    uint32_t deviceId = 0;
+    status = synDeviceAcquire(&deviceId, nullptr);
+    assert(status == synSuccess && "Failed to call  synDeviceAcquire()");
 
-    // synDeviceInfo deviceInfo;
-    // status = synDeviceGetInfo(deviceId, &deviceInfo);
-    // assert(status == synSuccess && "Failed to call  synDeviceGetInfo()");
+    synDeviceInfo deviceInfo;
+    status = synDeviceGetInfo(deviceId, &deviceInfo);
+    assert(status == synSuccess && "Failed to call  synDeviceGetInfo()");
 
-    // synDeviceType device_type = deviceInfo.deviceType;
+    synDeviceType device_type = deviceInfo.deviceType;
 
-    // // ************************************************ compile
-    // synGraphHandle graphHandle = nullptr;
-    // status = synGraphCreate(&graphHandle, device_type);
-    // assert(status == synSuccess && "Failed to call synGraphCreate()");
+    std::vector<std::string> dtypes = {"bf16", "f16", "f32"};
+    for (size_t j = 0; j < dtypes.size(); j++)
+    {
+        // ************************************************ compile
+        synGraphHandle graphHandle = nullptr;
+        status = synGraphCreate(&graphHandle, device_type);
+        assert(status == synSuccess && "Failed to call synGraphCreate()");
+        
+        std::vector<synTensor> inputs;
+        std::vector<synTensor> outputs;
+        
+        unsigned int offset = 0;
+        synDataType tensor_type = syn_type_fp16;
+        unsigned int data_size = 2;
+        
+        if (dtypes[j] == "bf16")
+        {
+            data_size = 2;
+            tensor_type = syn_type_bf16;
+        }
+        else if (dtypes[j] == "f16")
+        {
+            data_size = 2;
+            tensor_type = syn_type_fp16;
+        }
+        else if (dtypes[j] == "f32")
+        {
+            data_size = 4;
+            tensor_type = syn_type_single;
+        }
+        
+        // ************************************************ create X tensor
+        unsigned X_shape[] = {H, T, B};
+        unsigned X_size = H * T * B * data_size;
+        synSectionHandle X_SectionHandle = nullptr;
+        status = synSectionCreate(&X_SectionHandle, 0, graphHandle);
+        assert(status == synSuccess && "Failed to call synSectionCreate()");
+        
+        synTensorDescriptor X_desc;
+        X_desc.m_dataType = tensor_type;
+        X_desc.m_dims = 3UL;
+        X_desc.m_name = "X";
+        memset(X_desc.m_strides, 0, sizeof(X_desc.m_strides));
+        memset(X_desc.m_sizes, 0, sizeof(X_desc.m_sizes));
+        memcpy(X_desc.m_sizes, X_shape, 3 * sizeof(unsigned));
+        
+        synTensor syn_X = nullptr;
+        status = synTensorCreate(&syn_X, &X_desc, X_SectionHandle, offset);
+        assert(status == synSuccess && "Failed to call synTensorCreate()");
+        inputs.push_back(syn_X);
+        
+        //************************************************ create Y tensor
+        unsigned Y_shape[] = {H, T, B};
+        unsigned Y_size = H * T * B * data_size;
+        synSectionHandle Y_SectionHandle = nullptr;
+        status = synSectionCreate(&Y_SectionHandle, 0, graphHandle);
+        assert(status == synSuccess && "Failed to call synSectionCreate()");
+        
+        synTensorDescriptor Y_desc;
+        Y_desc.m_dataType = tensor_type;
+        Y_desc.m_dims = 3UL;
+        Y_desc.m_name = "Y";
+        memset(Y_desc.m_strides, 0, sizeof(Y_desc.m_strides));
+        memset(Y_desc.m_sizes, 0, sizeof(Y_desc.m_sizes));
+        memcpy(Y_desc.m_sizes, Y_shape, 3 * sizeof(unsigned));
+        synTensor syn_Y = nullptr;
+        
+        status = synTensorCreate(&syn_Y, &Y_desc, Y_SectionHandle, offset);
+        assert(status == synSuccess && "Failed to call synTensorCreate()");
+        outputs.push_back(syn_Y);
+        
+        //************************************************ create softmax node
+        std::string guid = "softmax_fwd_" + dtypes[j];
+        std::string node_name = "softmax_op";
+        
+        ns_Softmax::Params softmax_params;
+        softmax_params.dim = 0;
+    
+        status = synNodeCreate(graphHandle,
+                               inputs.data(),
+                               outputs.data(),
+                               1,
+                               1,
+                               &softmax_params,
+                               sizeof(softmax_params),
+                               guid.c_str(),
+                               node_name.c_str(),
+                               nullptr,
+                               nullptr);
+        assert(status == synSuccess && "Failed to call synNodeCreate()");
 
-    // std::vector<synTensor> inputs;
-    // std::vector<synTensor> outputs;
+        //************************************************ compile graph
+        synRecipeHandle recipeHandle = nullptr;
+        std::string name("softmax_" + dtypes[j] + ".recipe");
+        status = synGraphCompile(&recipeHandle,
+                                 graphHandle,
+                                 name.c_str(),
+                                 nullptr);
+        assert(status == synSuccess && "Failed to call synGraphCompile()");
 
-    // // ************************************************ create X tensor
-    // unsigned int offset = 0;
-    // unsigned X_shape[] = {H, T, B};
-    // unsigned X_size = H * T * B * 2;
-    // synSectionHandle X_SectionHandle = nullptr;
-    // status = synSectionCreate(&X_SectionHandle, 0, graphHandle);
-    // assert(status == synSuccess && "Failed to call synSectionCreate()");
+        status = synGraphDestroy(graphHandle);
+        assert(status == synSuccess && "Failed to call synGraphDestroy()");
+       
+        status = synRecipeDestroy(recipeHandle);
+        assert(status == synSuccess && "Failed to call synRecipeDestroy()");
+    }
 
-    // synTensorDescriptor X_desc;
-    // X_desc.m_dataType = syn_type_bf16;
-    // X_desc.m_dims = 3UL;
-    // X_desc.m_name = "X";
-    // memset(X_desc.m_strides, 0, sizeof(X_desc.m_strides));
-    // memset(X_desc.m_sizes, 0, sizeof(X_desc.m_sizes));
-    // memcpy(X_desc.m_sizes, X_shape, 3 * sizeof(unsigned));
+    // clean
+    status = synDeviceRelease(deviceId);
+    assert(status == synSuccess && "Failed to call synDeviceRelease()");
 
-    // synTensor syn_X = nullptr;
-    // status = synTensorCreate(&syn_X, &X_desc, X_SectionHandle, offset);
-    // assert(status == synSuccess && "Failed to call synTensorCreate()");
-    // inputs.push_back(syn_X);
-
-    // //************************************************ create C tensor
-    // unsigned Y_shape[] = {H, T, B};
-    // unsigned Y_size = H * T * B * 2;
-    // synSectionHandle Y_SectionHandle = nullptr;
-    // status = synSectionCreate(&Y_SectionHandle, 0, graphHandle);
-    // assert(status == synSuccess && "Failed to call synSectionCreate()");
-
-    // synTensorDescriptor Y_desc;
-    // Y_desc.m_dataType = syn_type_bf16;
-    // Y_desc.m_dims = 3UL;
-    // Y_desc.m_name = "Y";
-    // memset(Y_desc.m_strides, 0, sizeof(Y_desc.m_strides));
-    // memset(Y_desc.m_sizes, 0, sizeof(Y_desc.m_sizes));
-    // memcpy(Y_desc.m_sizes, Y_shape, 3 * sizeof(unsigned));
-    // synTensor syn_Y = nullptr;
-
-    // status = synTensorCreate(&syn_Y, &Y_desc, Y_SectionHandle, offset);
-    // assert(status == synSuccess && "Failed to call synTensorCreate()");
-    // outputs.push_back(syn_Y);
-
-    // //************************************************ create rms node
-    // std::vector<std::string> unary_guids = {"relu", "abs", "exp", "sigmoid", "sign", "sqrt", "tanh", "erf"."log", "neg", "floor", "ceil", "cos", "acos", "sin", "asin", "sinh", "tan", "softsign"};
-    // std::vector<std::string> dtypes = {"bf16", "f32", "f16"};
-    // for (size_t i = 0; i < unary_guids.size(); i++)
-    // {
-    //     for (size_t j = 0; j < dtypes.size(); j++)
-    //     {
-    //         std::string guid = unary_guids[i] + "_" + dtypes[j];
-    //         std::string guid = "softmax_fwd_bf16";
-    //         std::string node_name = guid + "_" + std::string(i) + "_" + std::string(j);
-    //         status = synNodeCreate(graphHandle,
-    //                                inputs.data(),
-    //                                outputs.data(),
-    //                                1,
-    //                                1,
-    //                                nullptr,
-    //                                sizeof(params),
-    //                                guid.c_str(),
-    //                                node_name.c_str(),
-    //                                nullptr,
-    //                                nullptr);
-    //         assert(status == synSuccess && "Failed to call synNodeCreate()");
-    //     }
-    // }
-
-    // //************************************************ compile graph
-    // synRecipeHandle recipeHandle = nullptr;
-    // std::string name("unary.recipe");
-    // status = synGraphCompile(&recipeHandle,
-    //                          graphHandle,
-    //                          name.c_str(),
-    //                          nullptr);
-    // assert(status == synSuccess && "Failed to call synGraphCompile()");
-
-    // // ************************************************ runtime
-    // uint64_t workspace = 0;
-    // status = synWorkspaceGetSize(&workspace, recipeHandle);
-    // assert(status == synSuccess && "Failed to call synWorkspaceGetSize()");
-
-    // uint64_t hbm_addr = 0;
-    // status = synDeviceMalloc(deviceId, workspace, 0, 0, &hbm_addr);
-    // assert(status == synSuccess && "Failed to call synDeviceMalloc()");
-
-    // std::vector<void *> host_inputs;
-    // std::vector<void *> host_outputs;
-
-    // // malloc host X tensor
-    // uint64_t X_device = 0;
-    // status = synDeviceMalloc(deviceId, X_size, 0, 0, &X_device);
-    // assert(status == synSuccess && "Failed to call X synDeviceMalloc()");
-
-    // // malloc device X tensor
-    // unsigned char *X_host = nullptr;
-    // status = synHostMalloc(deviceId, X_size, 0, (void **)&X_host);
-    // assert(status == synSuccess && "Failed to call X synHostMalloc()");
-    // memset(X_host, 0, X_size);
-    // host_inputs.push_back(X_host);
-
-    // // malloc device C tensor
-    // uint64_t Y_device = 0;
-    // status = synDeviceMalloc(deviceId, Y_size, 0, 0, &Y_device);
-    // assert(status == synSuccess && "Failed to call C synDeviceMalloc()");
-
-    // // malloc host C tensor
-    // unsigned char *Y_host = nullptr;
-    // status = synHostMalloc(deviceId, Y_size, 0, (void **)&Y_host);
-    // assert(status == synSuccess && "Failed to call C synHostMalloc()");
-    // memset(Y_host, 0, Y_size);
-    // host_outputs.push_back(Y_host);
-
-    // synStreamHandle h2d, d2h, compute;
-    // status = synStreamCreateGeneric(&h2d, deviceId, 0);
-    // assert(status == synSuccess && "Failed to create h2d");
-
-    // status = synStreamCreateGeneric(&d2h, deviceId, 0);
-    // assert(status == synSuccess && "Failed to create d2h");
-
-    // status = synStreamCreateGeneric(&compute, deviceId, 0);
-    // assert(status == synSuccess && "Failed to create compute");
-
-    // synEventHandle h2d_event = nullptr;
-    // status = synEventCreate(&h2d_event, deviceId, 0);
-    // assert(status == synSuccess && "Failed to call synEventCreate()");
-
-    // status = synMemCopyAsync(h2d, (uint64_t)X_host, X_size, X_device, HOST_TO_DRAM);
-    // assert(status == synSuccess && "Failed to call X synMemCopyAsync()");
-
-    // status = synEventRecord(h2d_event, h2d);
-    // assert(status == synSuccess && "Failed to call synEventRecord()");
-
-    // status = synEventQuery(h2d_event);
-    // std::cout << "h2d_event status: " << status << std::endl;
-
-    // status = synStreamWaitEvent(compute, h2d_event, 0);
-    // assert(status == synSuccess && "Failed to call synEventRecord()");
-
-    // synLaunchTensorInfoExt A_launch;
-    // A_launch.tensorName = "X";
-    // A_launch.pTensorAddress = X_device;
-
-    // synLaunchTensorInfoExt C_launch;
-    // C_launch.tensorName = "Y";
-    // C_launch.pTensorAddress = Y_device;
-
-    // std::vector<synLaunchTensorInfoExt> launch_info = {A_launch, C_launch};
-
-    // status = synLaunchExt(compute, launch_info.data(), launch_info.size(), hbm_addr, recipeHandle, 0);
-
-    // status = synStreamSynchronize(compute);
-    // assert(status == synSuccess && "Failed to call synStreamSynchronize()");
-
-    // // copy data from device to host
-    // status = synMemCopyAsync(d2h, Y_device, Y_size, (uint64_t)Y_host, DRAM_TO_HOST);
-    // assert(status == synSuccess && "Failed to call C synMemCopyAsync()");
-
-    // status = synStreamSynchronize(d2h);
-    // assert(status == synSuccess && "Failed to call synStreamSynchronize()");
-
-    // // query device status
-    // status = synEventQuery(h2d_event);
-    // std::cout << "h2d_event status: " << status << std::endl;
-
-    // status = synStreamQuery(compute);
-    // std::cout << "compute stream status: " << status << std::endl;
-
-    // // clean
-    // status = synEventDestroy(h2d_event);
-    // assert(status == synSuccess && "Failed to call synEventDestroy()");
-
-    // status = synGraphDestroy(graphHandle);
-    // assert(status == synSuccess && "Failed to call synGraphDestroy()");
-
-    // status = synRecipeDestroy(recipeHandle);
-    // assert(status == synSuccess && "Failed to call synRecipeDestroy()");
-
-    // status = synHostFree(deviceId, X_host, 0);
-    // assert(status == synSuccess && "Failed to call synHostFree()");
-
-    // status = synHostFree(deviceId, Y_host, 0);
-    // assert(status == synSuccess && "Failed to call synHostFree()");
-
-    // status = synStreamDestroy(h2d);
-    // assert(status == synSuccess && "Failed to call synStreamDestroy()");
-
-    // status = synStreamDestroy(d2h);
-    // assert(status == synSuccess && "Failed to call synStreamDestroy()");
-
-    // status = synStreamDestroy(compute);
-    // assert(status == synSuccess && "Failed to call synStreamDestroy()");
-
-    // status = synDeviceRelease(deviceId);
-    // assert(status == synSuccess && "Failed to call synDeviceRelease()");
-
-    // status = synDestroy();
-    // assert(status == synSuccess && "Failed to call synDestroy()");
+    status = synDestroy();
+    assert(status == synSuccess && "Failed to call synDestroy()");
     return 0;
 }
--- src/softmax/softmax.py
+++ src/softmax/softmax.py
@@ -1,6 +1,9 @@
 import os
 os.environ['ENABLE_EXPERIMENTAL_FLAGS'] = '1'
 os.environ['VISUALIZATION_MODE'] = '0'
+os.environ['GRAPH_VISUALIZATION'] = '1'
+os.environ['HABANA_LOGS'] = 'logs'
+os.environ['LOG_LEVEL_ALL'] = '0'
 
 import torch
 import torch.nn as nn
@@ -18,10 +21,27 @@ with torch.no_grad():
     hidden_states = nn.functional.softmax(x, dim=-1, dtype=torch.float32)
 
 ht.synchronize()
+
 x = torch.randn([B, T, H]).to(torch.bfloat16).to('hpu')
 ht.synchronize()
 
 with torch.no_grad():
     hidden_states = nn.functional.softmax(x, dim=-1, dtype=torch.bfloat16)
+
+ht.synchronize()
+
+x = torch.randn([B, T, H]).to(torch.float8_e4m3fn).to('hpu')
+ht.synchronize()
+
+with torch.no_grad():
+    hidden_states = nn.functional.softmax(x, dim=0, dtype=torch.float8_e4m3fn)
+
+ht.synchronize()
+
+x = torch.randn([B, T, H]).to(torch.float8_e5m2).to('hpu')
+ht.synchronize()
+
+with torch.no_grad():
+    hidden_states = nn.functional.softmax(x, dim=0, dtype=torch.float8_e5m2)
 
 ht.synchronize()
